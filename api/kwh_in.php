c9f539caf858b4f4c7249c0acc4ecd6a512f3b3d
tueseed <tue_seed@hotmail.com>
2018-12-18 11:01:07 +0700

    Add files via upload

--- api/kwh_in.php
+++ api/kwh_in.php
@@ -2,7 +2,7 @@
 	$method = $_SERVER["REQUEST_METHOD"];
 	if($method == "POST")
 	{
-	require('db/connect-db.php');
+	require('../db/connect-db.php');
 	$kwh = $_POST["kwh"];
 	$sql_insert_kwh = "INSERT INTO tbl_kwh(kwh) VALUES('$kwh')";
 	mysqli_query($conn,$sql_insert_kwh);
